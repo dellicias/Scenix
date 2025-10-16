e22dde89a1b3b449aba3cc6ab4dad0c8df08c0d4
Estéfany <m.estefany.sena@gmail.com>
2025-10-16 20:16:53 -0300

    3 - modificando os perfis 2 e 3

--- src/js/perfil2.js
+++ src/js/perfil2.js
@@ -1,18 +1,18 @@
 const bannerItems =[
     {
-        imagem: "../assets/img/sdaBnn.png",
+        imagem: "../assets/img/aneis.png",
         titulo:"O Senhor dos Anéis: os Anéis de Poder",
         descricao:"Milhares de anos antes dos eventos de O Hobbit e O Senhor dos Anéis. A trama explora a criação dos Anéis do Poder, a ascensão de Sauron, o reino de Númenor e a Última Aliança de Elfos e Homens."
 
     },
     {
-        imagem:"../assets/img/bbtBnn.png",
+        imagem:"../assets/img/bigbang.png",
         titulo:"Big Bang: A Teoria",
         descricao:"Leonard Hofstadter (Johnny Galecki) e Sheldon Cooper (Jim Parsons) são dois brilhantes físicos que dividem o mesmo apartamento. Suas vidas se complicam quando uma jovem linda, porém pouco inteligente, Penny (Kaley Cuoco), se muda para o apartamento da frente."
 
     },
     {
-        imagem:"../assets/img/swBnn.png",
+        imagem:"../assets/img/star.png",
         titulo:"Star Wars: Episódio IV – Uma Nova Esperança",
         descricao:"19 anos após os eventos de Star Wars - Episódio III: A Vingança dos Sith, o alto oficial do Império Galáctico e Lorde Sith, Darth Vader, sequestra a Senadora e Princesa Leia Organa sob duas acusações: fazer parte da resistência ao Império, a Aliança Rebelde, e interceptar os planos do projeto da arma secreta imperial, a Estrela da Morte."
 
--- src/js/perfil3.js
+++ src/js/perfil3.js
@@ -1,7 +1,7 @@
 const bannerItems =[
     {
         imagem: "../assets/img/perderhomemBnn.png",
-        titulo:"Como Perder Um HomemEm 10 Dias",
+        titulo:"Como Perder Um Homem Em 10 Dias",
         descricao:"Ben Barry é um publicitário que faz uma grande aposta com seu chefe: caso faça com que uma mulher se apaixone por ele em 10 dias ele será o responsável por uma concorrida campanha de diamantes que pertence à empresa. A vítima escolhida por Ben é Andie Anderson, uma jornalista feminista que está desenvolvendo uma matéria sobre como perder um homem em 10 dias e está decidida a infernizar a vida de qualquer homem que se aproximar dela."
 
     },
